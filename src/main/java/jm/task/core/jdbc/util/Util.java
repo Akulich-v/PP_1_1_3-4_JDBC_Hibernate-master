ba9646e7dd67055228556620d731f3f9eb8ea4c5
“Vladimir <Akulichvladimir94@gmail.com>
2024-03-19 00:18:57 +0300

    Настройка подключения

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtwst";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        Driver driver;
+
+        try {
+            driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+            System.err.println("Неудалось подключиться");
+        }
+        return connection;
+    }
+
+}
